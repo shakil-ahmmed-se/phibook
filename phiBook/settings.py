4298478b67a82dd0f8a9d28bd336eb1a31b2f0f5
shakil-ahmmed-se <mdshakilahmmed8906@gmail.com>
2024-08-31 04:13:21 -0700

    secret key added

--- phiBook/settings.py
+++ phiBook/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+
+SECRET_KEY='django-insecure-3b#h5zpgz@9!oz5-s=yi%38!y8%^y1yt*7$30($_t_(m$us(c%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
